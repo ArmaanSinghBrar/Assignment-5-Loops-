39b77c1fa0f1fa3f2f6e3bd66b69889330b63d81
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-07 14:07:09 +0530

    Create Question 4.py

---
+++ Question 4.py
@@ -0,0 +1,9 @@
+#Creating a given pattern
+for i in range(1,6):
+    for j in range(i):
+        print("* ",end="")
+    print("")
+for a in range(4,0,-1):
+    for b in range(a):
+        print("* ",end="")
+    print("")
