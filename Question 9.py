cbf2fde35106399a10bf0db6fd1eece2e32bf7ac
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-11 22:33:56 +0530

    Update Question 9.py

--- Question 9.py
+++ Question 9.py
@@ -1,11 +1,11 @@
-#Occurance of each word in a list entered by a user
-List = input("Enter a list").split()
-print("Count of each word in the list :")
-frequency = {}              #Creating an empty dictionary
-for Word in List:           #A loop for each word in the set
-  if Word in frequency:     #If word already in dictionary it adds one to the frequency
-    frequency[Word] += 1
-  else:                     #If word not in dictionary frequency is set to one
-    frequency[Word] = 1
-for Word,count in frequency.items():
-  print(f"{Word} occurs {count} times")
+#Number of occurrences of words or alphabets
+List=input("Enter a string").split()
+if len(List)>1:                #if the string is a sentence
+    for i in set(List):        #running a loop for all words in the string
+        Count=List.count(i)
+        print(f"Word {i} occurs {Count} times")
+elif len(List)==1:              #If the string is a word
+    word=List[0]
+    for i in set(word):         #running a loop for all alphabets in the word
+       Count=word.count(i)
+       print(f"Alphabet {i} occurs {Count} times")
