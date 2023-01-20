c5fe321abe206111a1be8c06e3c525ae7e8ef914
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-20 09:18:13 +0530

    Update Question 1.py

--- Question 1.py
+++ Question 1.py
@@ -1,4 +1,4 @@
 #Reversing a string
-string = str(input("Enter a string :"))
+string = input("Enter a string :")
 reverse_string = string[::-1]              #Reversing the string
 print(reverse_string)
